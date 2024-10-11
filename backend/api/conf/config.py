0a91bde695da70994b9d2b79ef5c8d016173cb2a
zhaoyi48 <zhaoyi48@outlook.com>
2024-10-11 22:31:22 +0800

    add miss model

--- backend/api/conf/config.py
+++ backend/api/conf/config.py
@@ -121,10 +121,12 @@ class OpenaiApiSetting(BaseModel):
     proxy: Optional[str] = None
     connect_timeout: int = Field(10, ge=1)
     read_timeout: int = Field(20, ge=1)
-    enabled_models: list[OpenaiApiChatModels] = ["gpt_3_5", "gpt_4", "o1_mini"]
+    enabled_models: list[OpenaiApiChatModels] = ["gpt_3_5", "gpt_4","gpt_4o", "o1_mini"]
     model_code_mapping: dict[OpenaiApiChatModels, str] = {
         "gpt_3_5": "gpt-3.5-turbo",
         "gpt_4": "gpt-4",
+        "gpt_4o": "gpt-4o",
+        "o1_mini": "o1_mini",
     }
 
 
